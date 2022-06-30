b28d7034c0299ddc23d796fa839ec6160ab4ffdc
James Macapagal <j.macapagal@gmail.com>
2022-06-30 11:36:47 -0500

    commit tree 2536db5d2f4104799f85e0b3114deafb76f9bc08

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,17 @@
-class Student 
+require_relative './user' 
+require_relative './teacher'
+class Student < User 
 
+    def initialize
+    @knowledge = []
+    end
+
+    def learn(string)
+     @knowledge << string
+     
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
